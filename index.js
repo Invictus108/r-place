4deb0554aca9959ef09e510c7d9e9e369f2b77d8
Invictus108 <jadencohen333@gmail.com>
2022-12-22 12:45:39 -0800

    make it better

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express"),
+    app = express(),
+    server = require("http").createServer(app),
+    io = require("socket.io")(server)
+
+    const c_rows = 75 //50
+    const c_cols = 75 //50
+
+    var canvas = []
+
+    for(var rows = 0; rows < c_rows; rows++){
+        canvas[rows] = []
+
+        for(var cols = 0; cols < c_cols; cols++){
+            canvas[rows][cols] = "#FFF"
+    }
+}
+
+app.use(express.static("public"))
+io.on("connection", socket => {
+    socket.emit("canvas", canvas)
+
+    socket.on("color", data => {
+        canvas[data.row - 1][data.col - 1] = data.color
+        io.emit("canvas", canvas)
+    })
+})
+
+server.listen(3000)
---
+++ public/place.js
@@ -0,0 +1,103 @@
+var time = 0;
+var isrun = false;
+var canClick = true;
+$(document).ready(() => {
+    var socket = io()
+
+    var canvas = $("#place")[0]
+    var contex = canvas.getContext("2d")
+
+    socket.on("canvas", canvasData =>{
+        canvasData.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                contex.fillStyle = col
+                contex.fillRect(colIndex * 10, rowIndex * 10, 10, 10) //size of squares. fillRect(x, y, width, height). 15
+
+            })
+        });
+    })
+
+
+    
+
+    $("#submit").click(() => {
+            socket.emit("color", {
+                col: parseInt($("#x-coord").val()),
+                row: parseInt($("#y-coord").val()),
+                color: $("#color").val()
+    
+            })
+            time = 10
+            
+            if (isrun == false) {
+                timer2()
+                isrun = true
+            }
+    })
+
+    function getMousePosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        document.getElementById("x-coord").value = parseInt(x/10 + 1) //15
+        document.getElementById("y-coord").value = parseInt(y/10 + 1) //15
+        if (canClick) {
+            socket.emit("color", {
+                col: parseInt($("#x-coord").val()),
+                row: parseInt($("#y-coord").val()),
+                color: $("#color").val()
+                
+            })
+            time = 10
+            
+            if (isrun == false) {
+                timer2()
+                isrun = true
+                canClick = false
+            }
+    }
+    }
+    
+    //let canvas = document.querySelector("canvas");
+    canvas.addEventListener("mousedown", 
+    function(e){
+        getMousePosition(canvas, e);
+    });
+    
+    
+    function timer2() {
+        //minutes * 60 se
+        
+       let refreshIntervalId = setInterval(updateCountdown, 1000); //update every 1 second
+    
+       function updateCountdown() {
+               const minutes = Math.floor(time / 60); // rounds a number DOWN to the nearest integer
+               let seconds = time % 60;
+    
+               seconds = seconds < 10 ? '0' + seconds : seconds; 
+               const contdownEl = document.getElementById("f1"); 
+               contdownEl.innerHTML = `${minutes}:${seconds}`;
+               
+    
+        if (isrun){
+           time--;
+           
+           document.getElementById("submit").style.visibility="hidden";  
+           if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
+            clearInterval(refreshIntervalId);
+            isrun = false
+            canClick = true
+            document.getElementById("submit").style.visibility="visible"; 
+
+           }
+          
+        }
+        }
+    
+      
+    }
+   
+})
+
+
+
